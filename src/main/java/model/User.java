3b24d58a649d96b207116b8e2e7fa21bcc56a6f4
MikiKru <michal_kruczkowski@o2.pl>
2020-06-06 11:55:46 +0200

    jdbc v8

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,28 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter             // automatycznie dodaje gettery do wszystkich pól klasowych
+@Setter             // automatycznie dodaje settery do wszystkich pól klasowych
+@ToString           // automatycznie dodaje metodę toString
 public class User {
+    private int userId;
+    private String userName;
+    private String userLastName;
+    private String userEmail;
+    private String userPassword;
+    private LocalDateTime userRegistration = LocalDateTime.now();
+    private boolean userStatus = true;
+
+    public User(String userName, String userLastName, String userEmail, String userPassword) {
+        this.userName = userName;
+        this.userLastName = userLastName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
 }
